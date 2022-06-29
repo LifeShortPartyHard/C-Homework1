ecce95a7926c6d6728677cbd47a9f18c88086a27
Dmitriy <nyx1889gmail.com>
2022-06-29 19:20:10 +0300

    added even

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("a = ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0) 
+{
+    Console.Write("Данное число является чётным");
+}
+else
+{
+    Console.Write("Данное число не является чётным");
+}

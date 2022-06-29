c56d7a6c426c19091411e4787f0d6544b4caf901
Dmitriy <nyx1889gmail.com>
2022-06-29 19:06:13 +0300

    added maxOf3

---
+++ Task 4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b & a > c) 
+{
+    Console.Write("max = " + a);
+}
+else
+{
+    if (b > c)
+    {
+        Console.Write("max = " + b);
+    }
+    else
+    {
+        Console.Write("max = " + c);
+    }    
+}

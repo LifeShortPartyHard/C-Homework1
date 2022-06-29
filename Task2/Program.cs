d0370e25eb48aed32bec1a900764761fdc7a8161
Dmitriy <nyx1889gmail.com>
2022-06-29 18:40:26 +0300

    added maxOf2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("a = ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("b = ");
+double b = Convert.ToDouble(Console.ReadLine());
+if (a > b) 
+{
+    Console.Write("max = " + a);
+}
+else
+{
+    Console.Write("max = " + b);
+}
